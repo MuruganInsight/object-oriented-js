3ae974611680c1533d936035f176c8f275913715
“MuruganInsight” <muruganInsight@rrd.com>
2018-10-23 09:07:09 +0530

    Objects with delete

--- js/main.js
+++ js/main.js
@@ -1,1 +1,15 @@
-window.alert("hello world");
+const cat = {
+    name: 'Bailey',
+    age: 2,
+    meow(){
+      console.log('Hello world')
+    },
+    sayName(name){
+      console.log(`Hello ${name}`)
+    }
+  }
+  
+  cat.sayName('Murugan');
+  delete cat.sayName
+  
+  console.log(cat)
